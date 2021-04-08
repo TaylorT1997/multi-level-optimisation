d67d2e500c90001225ffe92709c46af71824b137
taylort1997 <taylort1997@outlook.com>
2021-04-08 16:47:26 +0100

    Implemented sentence level dataset for prediction

--- TODO.md
+++ TODO.md
@@ -12,6 +12,7 @@
 ## Low Priority
 
 - [ ] Google doc for papers
+- [ ] Write up data processing progress
 
 # Blockers
 
@@ -25,3 +26,19 @@
 ## Low Priority
 
 
+# Questions
+
+## High Priority
+
+
+## Medium Priority
+
+- Is it better to add unknown words to vocab or split into tokens?
+- How are split tokens dealt with during testing?
+
+## Low Priority
+
+
+# Notes
+
+- Remember to change the size of the embedding matrix after increasing the number of tokens! (https://github.com/huggingface/transformers/issues/1413#issuecomment-538083512, https://github.com/huggingface/tokenizers/issues/247#issuecomment-675458087)
--- src/data_loading/datasets.py
+++ src/data_loading/datasets.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import numpy as np
 
 import torch
 from torch.utils.data import Dataset
@@ -27,75 +28,93 @@ class BinaryTSVDataset(Dataset):
             tsv_file = os.path.join(
                 data_dir, "conll_10_{}_{}.tsv".format(conll_10_type, mode)
             )
-            positive_label = "C"
+            self.positive_label = "C"
 
         elif dataset_name == "fce":
             data_dir = os.path.join(root_dir, "data", "processed", "fce_v2.1")
             tsv_file = os.path.join(data_dir, "fce_{}.tsv".format(mode))
-            positive_label = "i"
+            self.positive_label = "i"
 
         elif dataset_name == "toxic":
             data_dir = os.path.join(root_dir, "data", "processed", "toxic")
             tsv_file = os.path.join(data_dir, "tsd_{}.tsv".format(mode))
-            positive_label = "t"
+            self.positive_label = "t"
 
         elif dataset_name == "wi_locness":
             data_dir = os.path.join(root_dir, "data", "processed", "wi_locness_v2.1")
             tsv_file = os.path.join(
                 data_dir, "wi_locness_{}_{}.tsv".format(wi_locness_type, mode)
             )
-            positive_label = "i"
+            self.positive_label = "i"
 
         else:
             raise Exception(
                 "No valid dataset selected, please use one of: {}".format(datasets)
             )
 
-        self._get_samples(tsv_file)
+        self.samples, self.labels = self._get_samples_labels(tsv_file)
+        self.max_length = self._get_max_length(self.samples)
 
     def __len__(self):
-        return None
+        return len(self.samples)
 
-    def __get_item__(self, idx):
-        return None
+    def __getitem__(self, idx):
+        sequence = self.samples[idx]
+        label = self.labels[idx]
 
-    def _get_samples(self, tsv_file):
+        encoded_sequence = self.tokenizer.encode_plus(
+            sequence, padding="max_length", truncation=True, max_length=self.max_length
+        )
+
+        return encoded_sequence, label
+
+    def _get_samples_labels(self, tsv_file):
         samples = []
+        sentence_labels = []
 
         with open(tsv_file, "r", encoding="utf-8") as tsv:
             tsv_reader = csv.reader(tsv, delimiter="\t")
 
             words = []
-            labels = []
+            word_labels = []
 
             for row in tsv_reader:
                 if row:
                     words.append(row[0])
-                    labels.append(row[1])
+                    label = 1 if row[1] == self.positive_label else 0
+                    word_labels.append(label)
+
                 else:
-                    break
+                    if words:
+                        sentence_label = max(word_labels)
+                        tokenized_words = self._tokenize_input(words)
 
-            print(words)
-            print(labels)
+                        samples.append(tokenized_words)
+                        sentence_labels.append(sentence_label)
 
-            print()
+                    words = []
+                    word_labels = []
+                    continue
+
+        return samples, sentence_labels
 
     def _tokenize_input(self, words):
+        encoded_words = []
 
         for word in words:
-            encoded_word = tokenizer.encode(word)
+            encoded_word = tokenizer.encode(word, add_special_tokens=False)
+            encoded_words.append(encoded_word)
 
-        return tokenized_words
+        encoded_output = [word for sublist in encoded_words for word in sublist]
+        return encoded_output
 
-    def _add_masked_tokens(self,):
+    def _add_masked_tokens(self):
         pass
 
-
-# dataset = BinaryTSVDataset(dataset_name="wi_locness")
-
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-
-text = "yo [MASK]"
-
-encoding = tokenizer.encode()
-print(encoding)
+    def _get_max_length(self, sentences):
+        max_length = 0
+        for sentence in sentences:
+            length = len(sentence)
+            if length > max_length:
+                max_length = length
+        return max_length
